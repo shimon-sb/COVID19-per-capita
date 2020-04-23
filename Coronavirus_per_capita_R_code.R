9d584386d3cd7731ee7d8f7d3723d25d9c032406
Shimon Saphire-Bernstein <shimon.saphirebernstein@gmail.com>
2020-04-23 15:50:01 -0500

    Select most recent file, update comments

--- Coronavirus_per_capita_R_code.R
+++ Coronavirus_per_capita_R_code.R
@@ -1,15 +1,17 @@
-##Coronavirus rolling per-capita updates file (an experiment)
-#Read in wikipedia file on country populations
-#install.packages("rvest")
-library(rvest)
-#library(xml2)
-library(dplyr)
-#library(stringr)
+####Coronavirus rolling per-capita updates file
 
-##Web-scraping of Wikipedia table performed following the instructions found at
-##https://www.engineeringbigdata.com/web-scraping-wikipedia-world-population-rvest-r/,
-##using the packages "rvest" and "xml2" and the magrittr pipe (here accessed
-##using dplyr)
+#Loading required libraries (make sure these are already installed!)
+library(rvest)
+library(dplyr)
+library(stringr)
+
+###Extracting the population values for individual countries
+
+#This first block of code extracts the population sizes of countries in the world
+#from Wikipedia (link below) so that they can be combined with case and death reports
+#Web-scraping of the Wikipedia table is performed following the guidance at
+#https://www.engineeringbigdata.com/web-scraping-wikipedia-world-population-rvest-r/,
+#using the package "rvest" and the "magrittr" pipe (here accessed using "dplyr")
 wiki_pop <- "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
 wiki_pop <- read_html(wiki_pop)
 global_pop <- wiki_pop %>%
@@ -17,7 +19,7 @@ global_pop <- wiki_pop %>%
   html_table()
 global_pop <- global_pop[[1]]
 
-#Cleaning the global_pop table
+#Cleaning the global_pop data frame
 names(global_pop) <- c("Rank", "Country", "Population", "Percent", "Date", "Source")
 #Removing bracketed notes using the gsub function
 global_pop$Country <- gsub('\\[[^]]*\\]', '', global_pop$Country)
@@ -37,29 +39,36 @@ global_pop$country_pooled <- sub('\\)', '', global_pop$country_pooled)
 global_pop$country_pooled <- sub('US', 'United States', global_pop$country_pooled)
 global_pop$country_pooled <- sub('UK', 'United Kingdom', global_pop$country_pooled)
 global_pop$country_pooled <- sub('NZ', 'New Zealand', global_pop$country_pooled)
+
 #Removes commas from Population and converts it to numeric
 global_pop$Population <- gsub(',', '', global_pop$Population)
 global_pop$Population <- as.numeric(global_pop$Population)
-#Collapses population of territories into the total for the sovereign nation
+
+#Collapses population of territories into the total for each sovereign nation
 pop_by_country <- global_pop %>% group_by(., country_pooled) %>%
   summarize(., Population = sum(Population)) %>%
   ungroup(.)
 names(pop_by_country) <- c("Country", "Population")
-#Renames some pesky countries to conform with the COVID-19 dataset
-pop_by_country$Country <- sub("^Congo$", "Congo (Brazzaville)", pop_by_country$Country)
-pop_by_country$Country <- sub("DR Congo", "Congo (Kinshasa)", pop_by_country$Country)
 
+###Extracting the coronavirus cases by country from the Johns Hopkins site
 
-##Extracting the coronavirus cases by country from the Johns Hopkins site
-#Johns Hopkins data can be accessed at https://github.com/CSSEGISandData/COVID-19
-#For now, the data are stored in Data_science/COVID-19
-setwd("C:/Users/shim/Desktop/Data_Science/COVID-19")
-#Loading the data from 3/29/20
-covid_19_data <- read.csv("csse_covid_19_data/csse_covid_19_daily_reports/03-29-2020.csv")
+#First, pull the latest update from https://github.com/CSSEGISandData/COVID-19.git
+#and set the working directory to wherever you have downloaded these files
+#This block of code will then locate and load the most recent daily report file:
+setwd("csse_covid_19_data/csse_covid_19_daily_reports")
+covid_files <- list.files()
+#File names are simply the date of upload, in MM-DD-YYYY format, so a decreasing
+#sort will put the latest file at the top of the list (after the ReadMe)
+covid_files <- str_sort(covid_files, decreasing = TRUE)
+current_file <- covid_files[2]
+covid_19_data <- read.csv(current_file)
+
+#Collapsing the data across states/provinces/etc within countries
 covid_19_by_country <- covid_19_data %>% group_by(., Country_Region) %>%
   summarize(., Cases = sum(Confirmed), Deaths = sum(Deaths)) %>%
   ungroup(.)
 names(covid_19_by_country) = c("Country", "Cases", "Deaths")
+
 #Fixing a few country names in the COVID-19 dataset
 covid_19_by_country$Country <- sub('US', 'United States', covid_19_by_country$Country)
 covid_19_by_country$Country <- sub("\\*", "", covid_19_by_country$Country)
@@ -69,20 +78,25 @@ covid_19_by_country$Country <- sub("Timor-Leste", "East Timor", covid_19_by_coun
 covid_19_by_country$Country <- sub("Czechia", "Czech Republic", covid_19_by_country$Country)
 covid_19_by_country$Country <- sub("Cote d'Ivoire", "Ivory Coast", covid_19_by_country$Country)
 covid_19_by_country$Country <- sub("Cabo Verde", "Cape Verde", covid_19_by_country$Country)
+covid_19_by_country$Country <- sub("Congo (Brazzaville)", "Congo", covid_19_by_country$Country)
+covid_19_by_country$Country <- sub("Congo (Kinshasa)", "DR Congo", covid_19_by_country$Country)
 
 #Merging COVID-19 data with population data
-global_merged <- merge(pop_by_country, covid_19_by_country, by = "Country", all = T)
+global_merged <- merge(pop_by_country, covid_19_by_country, by = "Country", all = TRUE)
 global_merged <- na.omit(global_merged)
+
 #Computing the per capita rates of incidence and death (and their inverses)
 global_merged$Cases_pc <- global_merged$Cases / global_merged$Population
 global_merged$Deaths_pc <- global_merged$Deaths / global_merged$Population
 global_merged$Cases_1_per <- 1 / global_merged$Cases_pc
 global_merged$Deaths_1_per <- 1 / global_merged$Deaths_pc
+global_merged$pct_total_cs <- global_merged$Cases / sum(global_merged$Cases)
 
 #Calculating rate of deaths per number of confirmed cases
 global_merged$death_rate = global_merged$Deaths / global_merged$Cases
 
-##Extracting population data for states in the US
+###Extracting population data for states in the US
+
 wiki_pop_us <- "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States_by_population"
 wiki_pop_us <- read_html(wiki_pop_us)
 us_pop <- wiki_pop_us %>%
@@ -94,7 +108,9 @@ names(us_pop) <- c("State", "Population")
 us_pop <- us_pop[2:57,]
 us_pop$State <- sub("U.S. Virgin Islands", "Virgin Islands", us_pop$State)
 
-#Prepping the U.S. states subset and merging with population by state
+###Prepping the subset of COVID-19 data for the U.S. states and merging it
+###with population by state
+
 covid_19_us <- subset(covid_19_data, Country_Region == "US")
 us_by_state <- covid_19_us %>% group_by(., Province_State) %>%
   summarize(., Cases = sum(Confirmed), Deaths = sum(Deaths)) %>%
@@ -103,9 +119,10 @@ names(us_by_state) <- c("State", "Cases", "Deaths")
 us_merged <- merge(us_pop, us_by_state, by = "State", all = T)
 us_merged <- na.omit(us_merged)
 
+#Calculating various summary statistics, including per captia rates (and their
+#inverses) and death rate
 us_merged$Population <- gsub(',', '', us_merged$Population)
 us_merged$Population <- as.numeric(us_merged$Population)
-us_merged$Cases <- as.numeric(us_merged$Cases)
 us_merged$Cases_pc <- us_merged$Cases / us_merged$Population
 us_merged$Cases_1_per <- 1 / us_merged$Cases_pc
 us_merged$Deaths_pc <- us_merged$Deaths / us_merged$Population

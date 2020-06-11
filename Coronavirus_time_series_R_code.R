37fc24fc72ef98b63d7c8c333242499bef73aebc
Shimon Saphire-Bernstein <shimon.saphirebernstein@gmail.com>
2020-06-11 15:59:00 -0500

    US data - Dropped unnecessary columns and collapsed remaining columns by state or province

--- Coronavirus_time_series_R_code.R
+++ Coronavirus_time_series_R_code.R
@@ -4,6 +4,10 @@
 #the calculation of simple stats like daily increases, etc., by country or U.S.
 #state and the creation of plots of the curves for cases, deaths etc.
 
+##Loading required libraries
+library(dplyr)
+library(stringr)
+
 ###Loading the data - assumes the data have been cloned from the Johns Hopkins
 ###website's github (https://github.com/CSSEGISandData/COVID-19.git) and that
 ###the working directory has been set to the master branch of the cloned git
@@ -12,3 +16,18 @@ us_cases_ts <- read.csv("time_series_covid19_confirmed_US.csv")
 us_deaths_ts <- read.csv("time_series_covid19_deaths_us.csv")
 global_cases_ts <- read.csv("time_series_covid19_confirmed_global.csv")
 global_deaths_ts <- read.csv("time_series_covid19_deaths_global.csv")
+
+###Collapsing US data by state or province
+#First removing unnecessary columns
+drop <- c("UID", "iso2", "iso3", "code3", "FIPS", "Admin2", "Country_Region",
+          "Lat", "Long_", "Combined_Key")
+us_cases_ts <- us_cases_ts[, !(names(us_cases_ts) %in% drop)]
+us_deaths_ts <- us_deaths_ts[, !(names(us_deaths_ts) %in% drop)]
+
+#Then aggregating by state or province for all other columns
+us_cases_ts <- us_cases_ts %>% group_by(., Province_State) %>%
+  summarize_all(., sum) %>%
+  ungroup(.)
+us_deaths_ts <- us_deaths_ts %>% group_by(., Province_State) %>%
+  summarize_all(., sum) %>%
+  ungroup(.)
